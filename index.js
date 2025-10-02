a82efdb7bbd6505990d4409bca17aa75f3e51b4c
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-10-02 14:46:01 +0300

    changed index.js to use models

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const express = require("express");
 const app = express();
@@ -10,10 +10,35 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {}, // chaged this line from course material because the docker container does not seem to work otherwise.
 });
 
+class Note extends Model { }
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  },
+);
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll()
   res.json(notes);
 });
 const PORT = process.env.PORT || 3001;
