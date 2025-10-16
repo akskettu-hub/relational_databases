8b46300a947fb84105f50d1105650568b4a1a7bf
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-10-16 13:54:36 +0300

    ex 13.12 retrieving users and blogs includes info about the other.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,7 +5,13 @@ const { Blog, User } = require("../models");
 const { SECRET } = require("../util/config");
 
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name", "username"],
+    },
+  });
   res.json(blogs);
 });
 
