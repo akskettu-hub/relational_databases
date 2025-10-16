dbd78a16d10d520a585ba65be3501fa818270c27
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-10-16 14:09:49 +0300

    ex 13.13. Implement filtering by keyword in blog title field.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,24 @@ const jwt = require("jsonwebtoken");
 
 const { Blog, User } = require("../models");
 const { SECRET } = require("../util/config");
+const { Op } = require('sequelize')
 
 router.get("/", async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search,
+    };
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name", "username"],
     },
+    where,
   });
   res.json(blogs);
 });
