f9fa4e7ac5246d57a5c2ce33689daffca6c1e9fd
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-10-22 14:36:09 +0300

    ex 13.19. Add reading list by migration.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./reading_list")
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList, as: 'user_reading_list' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'on_users_reading_list' })
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 };
