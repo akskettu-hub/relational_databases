e84eee85acab72518b0d9dc3cb39e27616eeba38
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-10-22 16:45:40 +0300

    ex 13.22. Implement functionality for updating read mark for reading list items.

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,12 +1,36 @@
 const router = require("express").Router();
 const { ReadingList } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.post("/", async (req, res, next) => {
   try {
-    const { userId, blogId } = req.body
+    const { userId, blogId } = req.body;
 
-    const newEntry = await ReadingList.create({ userId, blogId })
-    res.status(201).json(newEntry)
+    const newEntry = await ReadingList.create({ userId, blogId });
+    res.status(201).json(newEntry);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", tokenExtractor, async (req, res, next) => {
+  const readingListEntry = await ReadingList.findByPk(req.decodedToken.id);
+
+  try {
+    if (readingListEntry && readingListEntry.userId === req.decodedToken.id) {
+      const { read } = req.body;
+      if (read !== undefined) {
+        readingListEntry.readMark = read;
+        const updatedEntry = await readingListEntry.save();
+        res.json(updatedEntry);
+      } else {
+        res.status(400), json({ error: "Value of read missing." })
+      }
+    } else {
+      res
+        .status(400)
+        .json({ error: "Read status can only be modified by associated user" });
+    }
   } catch (error) {
     next(error);
   }
--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,21 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("./config");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    console.log(authorization);
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
 const unknowEndpoint = (request, response) => {
   response.status(404).send({ error: "unknow endpoint" });
 };
@@ -32,4 +50,5 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   unknowEndpoint,
   errorHandler,
+  tokenExtractor,
 };
